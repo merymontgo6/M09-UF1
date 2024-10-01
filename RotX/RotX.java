9105cca7fb1d4ece0120ab4eb282fa68aada48f7
merymontgo6 <karolayn.mruiz@gmail.com>
2024-10-01 23:45:26 +0200

    act2 avance

--- RotX/RotX.java
+++ RotX/RotX.java
@@ -30,11 +30,11 @@ public class RotX {
         for(int i = 0; i < text.length();i++){
             char textDesxifrat = text.charAt(i);
             if(Character.isLowerCase(textDesxifrat)){
-                int pos = (textDesxifrat - 'a' - num)%26;
+                int pos = (textDesxifrat - 'a' - num)%minus.length;
                 str.append((minus[pos]));
             } 
             else if (Character.isUpperCase(textDesxifrat)){
-                int pos = (textDesxifrat - 'A' - num)%26;
+                int pos = (textDesxifrat - 'A' - num)%majus.length;
                 str.append(majus[pos]);
             }
             else {
@@ -43,9 +43,38 @@ public class RotX {
         } return str.toString();
     }
 
-    //public static String forcaBrutaRotX(String textXifrat) {
+// Ha de provar tots els desplaçaments possibles i mostrar el missatge
+// resultant de desxifrar amb desplaçaments de 1,2,3,... fins la longitud de l’abecedari.
+public static String forcaBrutaRotX(String text) {
+    StringBuilder str = new StringBuilder();
 
-    //}
+    for (int i = 1; i <= minus.length; i++) { // 0 no pq seria lo mismo xd 
+        StringBuilder str2 = new StringBuilder();
+        
+        for (int j = 0; j < text.length(); j++) {
+            char c = text.charAt(j);
+
+            if (Character.isLowerCase(c)) {
+                for (int k = 0; k < minus.length; k++) {
+                    if (minus[k] == c) {
+                        str2.append(minus[(k + i) % minus.length]);
+                        break;
+                    }
+                }
+            } else if (Character.isUpperCase(c)) {
+                for (int h = 0; h < majus.length; h++) {
+                    if (majus[h] == c) {
+                        str2.append(majus[(h + i) % majus.length]);
+                        break;
+                    }
+                }
+            } else {
+                str2.append(c);
+            }
+        }
+        str.append("Desplaçament " + i + ": " + str2.toString() + "\n");
+    } return str.toString();
+}
 
     public static void main(String[] args) throws Exception {
         try (Scanner scanner = new Scanner(System.in)) {
