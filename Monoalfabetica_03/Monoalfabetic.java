962ae6843ff4caf0ddca88381314f849a2f6749b
Karolayn Muñoz <karolayn.mruiz@gmail.com>
2024-10-10 13:19:22 +0200

    avance act3

--- Monoalfabetica_03/Monoalfabetic.java
+++ Monoalfabetica_03/Monoalfabetic.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -6,12 +7,12 @@ public class Monoalfabetic {
     private static final char[] minus = "aàáâäãbcçdeèéêëfghiìíîïjklmnñopqrstuùúûüvwxyz".toCharArray();
     private static final char[] majus = "AÀÁÂÄÃBCÇDEÈÉÊËFGHIÌÍÎÏJKLMNÑOPQRSTUÙÚÛÜVWXYZ".toCharArray();
 
-    public static char[] permutaAlfabet(String alfabet) {
+    public static char[] permutAlfabet() {
         List<Character> canvi = new ArrayList<>();
         for(int i = 0; i < minus.length; i++){
             canvi.add(minus[i]);
         }
-        System.out.println("Alfabet original: " + minus);
+        System.out.println("Alfabet original: " + Arrays.toString(minus));
         Collections.shuffle(canvi);
         System.out.println("Alfabet permutat: " + canvi);
 
@@ -20,5 +21,48 @@ public class Monoalfabetic {
             alfabetP[i] = canvi.get(i);
         }
         return alfabetP;
+    }
+
+    private static int findIndex(char[] canvi, char lletra) {
+        for (int i = 0; i < canvi.length; i++) {
+            if (canvi[i] == lletra) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static String xifraMonoAlfa(String cadena) {
+    char[] alfabetPermutat = permutAlfabet();
+    StringBuilder cadenaXifrada = new StringBuilder();
+
+    for (int i = 0; i < cadena.length(); i++) {
+        char lletra = cadena.charAt(i);
+        if (Character.isLowerCase(lletra)) {
+            int index = findIndex(minus, lletra);
+            if (index != -1) {
+                cadenaXifrada.append(alfabetPermutat[index]);
+            } else {
+                cadenaXifrada.append(lletra);
+            }
+        } else if (Character.isUpperCase(lletra)) {
+            int index = findIndex(majus, lletra);
+            if (index != -1) {
+                cadenaXifrada.append(Character.toUpperCase(alfabetPermutat[index]));
+            } else {
+                cadenaXifrada.append(lletra);
+            }
+        } else {
+            cadenaXifrada.append(lletra);
+        }
+    }
+    return cadenaXifrada.toString();
+    }
+
+    public static void main(String[] args) {
+        String cadena = "Hola, como estas?";
+        String cadenaXifrada = xifraMonoAlfa(cadena);
+        System.out.println("Texto original: " + cadena);
+        System.out.println("Texto xifrat: " + cadenaXifrada);
     }
 }
